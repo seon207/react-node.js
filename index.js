4d1e90c981d772869f344bef133ec0248e188fad
Seon <seon@Seons-MacBook-Air.local>
2023-07-14 01:45:31 +0900

    정보 보호 코드 추가

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {User} = require("./models/User")
+const bodyParser = require('body-parser')
+const config = require('./config/key')
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
 
+//mongodb 연결
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seon:<mvseon>@moviecluster.k5wwmg8.mongodb.net/',{
-
+mongoose.connect(config.mongoURI,{
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕안녕')
 })
+app.post('/register', async(req, res)=>{
+  //회원가입 할 때 필요한 정보를 client에서 가져오면
+  //그것들을 데이터베이스에 넣어줌
 
+  const user = new User(req.body)
+  //mongodb method
+  const result = await user.save().then(()=>{
+    res.status(200).json({
+      success: true
+    })
+  }).catch((err)=>{
+    res.json({ success: false, err })
+  })
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//nodemon -> 소스 변경하면 자동으로 감지해서 실행, 서버 껐다 켰다 안해도 됨
--- models/User.js
+++ models/User.js
@@ -1,1 +1,32 @@
-const mongoose = require('mongose')
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        maxlength:50
+    },
+    email:{
+        type:String,
+        trim: true, //스페이스바 없애기
+        unique:1        
+    },
+    password:{
+        type:String,
+        maxlength:50
+    },
+    role:{
+        type: Number,
+        default:0
+    },
+    image:String,
+    token:{
+        type:String
+    },
+    tokenExp:{
+        type:Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {User} //다른 곳에서 사용할 수 있도록 export
